8175f3ed8af7beccf851fb92cabb648b77ddb9b7
Mary Grace Lucas <graceylucas@Marys-MacBook-Air.local>
2015-08-15 22:31:16 -0400

    Creates balloon struct

---
+++ 99BalloonsChallenge/Balloon.swift
@@ -0,0 +1,19 @@
+//
+//  Balloon.swift
+//  99BalloonsChallenge
+//
+//  Created by Mary Grace Lucas on 8/15/15.
+//  Copyright (c) 2015 Mary Grace Lucas. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+struct Balloon {
+    var balloonImage:UIImage
+    var balloonNumber:Int
+    
+    
+
+}
